e76111d0f2b461be1e60d56858c854d4190c9a54
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-07-18 15:08:15 +0200

    publish Data NFT and a Datatoken with a liquidity pool

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,16 @@
 import {
-  Config,
-  ProviderInstance,
   Aquarius,
   NftFactory,
   NftCreateData,
-  Datatoken,
-  Nft,
   ZERO_ADDRESS,
   Erc20CreateParams,
-  Files,
   transfer,
-  ComputeAsset,
-  ComputeAlgorithm,
-  ComputeJob,
-  sleep,
-  ProviderComputeInitialize,
-  ConsumeMarketFee,
-  approveWei,
   configHelperNetworks,
   ConfigHelper
 } from '@oceanprotocol/lib'
 import Web3 from 'web3';
+import fs from 'fs'
+import { homedir } from 'os'
 
 const web3 = new Web3(process.env.NODE_URI || configHelperNetworks[1].nodeUri)
 
@@ -30,16 +20,74 @@ const getTestConfig = async (web3: Web3) => {
   return config
 }
 
+const getAddresses = () => {
+  const data = JSON.parse(
+    // eslint-disable-next-line security/detect-non-literal-fs-filename
+    fs.readFileSync(
+      process.env.ADDRESS_FILE ||
+        `${homedir}/.ocean/ocean-contracts/artifacts/address.json`,
+      'utf8'
+    )
+  )
+  return data.development
+}
+
 /**
  * main function
  */
 async function main() {
+  // load the configuration
   const config = await getTestConfig(web3)
   const aquarius = new Aquarius(config.metadataCacheUri)
   const providerUrl = config.providerUri
 
   console.log(`Aquarius URL: ${config.metadataCacheUri}`)
   console.log(`Provider URL: ${providerUrl}`)
+
+  // initialize accounts
+  const accounts = await web3.eth.getAccounts()
+  const publisherAccount = accounts[0]
+  const consumerAccount = accounts[1]
+
+  console.log(`Publisher account address: ${publisherAccount}`)
+  console.log(`Consumer account address: ${consumerAccount}`)
+
+  // get the address of the deployed contracts
+  const addresses = getAddresses()
+
+  // send some OCEAN to consumer account
+  transfer(web3, publisherAccount, addresses.Ocean, consumerAccount, '100')
+
+  // publish Data NFT and a Datatoken with a liquidity pool
+
+  const factory = new NftFactory(addresses.ERC721Factory, web3)
+
+  const nftParamsAsset: NftCreateData = {
+    name: 'NAME',
+    symbol: 'SYMBOL',
+    templateIndex: 1,
+    tokenURI: 'aaa',
+    transferable: true,
+    owner: publisherAccount
+  }
+  const erc20ParamsAsset: Erc20CreateParams = {
+    templateIndex: 1,
+    cap: '100000',
+    feeAmount: '0',
+    paymentCollector: ZERO_ADDRESS,
+    feeToken: ZERO_ADDRESS,
+    minter: publisherAccount,
+    mpFeeAddress: ZERO_ADDRESS
+  }
+
+  const tx = await factory.createNftWithErc20(publisherAccount, nftParamsAsset, erc20ParamsAsset)
+
+  const nftAddress = tx.events.NFTCreated.returnValues[0]
+  const datatokenAddress = tx.events.TokenCreated.returnValues[0]
+
+  
+  console.log(`NFT address: ${nftAddress}`)
+  console.log(`Datatoken address: ${datatokenAddress}`)
 }
 
 main()
