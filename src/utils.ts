59ca39215c63bbfdb756bb6e8ec398d6119cdf5c
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-08-02 00:00:20 +0200

    add types to utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import { web3 } from './web3'
 
-export const getCurrentAccount = async () => {
+export const getCurrentAccount = async (): Promise<string> => {
   const accounts = await web3.eth.getAccounts()
   return accounts[0]
 }
