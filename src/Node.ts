6931e9e32da9d7973839b0ba53a3806654ded82d
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-08-02 00:10:24 +0200

    add Node code

---
+++ src/Node.ts
@@ -0,0 +1,86 @@
+import { Config, Nft, setContractDefaults } from '@oceanprotocol/lib'
+import Web3 from 'web3'
+
+export const INBOUND_KEY = 'inbound_addrs'
+export const OUTBOUND_KEY = 'outbound_addrs'
+
+export class Node extends Nft {
+  private nftAddress: string
+
+  constructor(
+    nftAddress: string,
+    web3: Web3,
+    network?: string | number,
+    config?: Config
+  ) {
+    super(web3, network, null, config)
+    this.nftAddress = nftAddress
+  }
+
+  public async name(): Promise<string> {
+    const nftContract = setContractDefaults(
+      new this.web3.eth.Contract(this.nftAbi, this.nftAddress),
+      this.config
+    )
+    return await nftContract.methods.name().call()
+  }
+
+  public async symbol(): Promise<string> {
+    const nftContract = setContractDefaults(
+      new this.web3.eth.Contract(this.nftAbi, this.nftAddress),
+      this.config
+    )
+    return await nftContract.methods.symbol().call()
+  }
+
+  // ==== inbounds ====
+
+  public async getInboundAddrs(): Promise<string[]> {
+    return await this._getAddrs(INBOUND_KEY)
+  }
+
+  public async addInboundNode(account: string, node: Node): Promise<void> {
+    await this.addInboundAddr(account, node.nftAddress)
+  }
+
+  public async addInboundAddr(account: string, nodeAddress: string): Promise<void> {
+    await this._addAddr(account, INBOUND_KEY, nodeAddress)
+  }
+
+  // ==== outbounds ====
+
+  public async getOutboundAddrs(): Promise<string[]> {
+    return await this._getAddrs(OUTBOUND_KEY)
+  }
+
+  public async addOutboundNode(account: string, node: Node): Promise<void> {
+    await this.addOutboundAddr(account, node.nftAddress)
+  }
+
+  public async addOutboundAddr(account: string, nodeAddress: string): Promise<void> {
+    await this._addAddr(account, OUTBOUND_KEY, nodeAddress)
+  }
+
+  // ==== helpers ====
+
+  public async _getAddrs(key: string): Promise<string[]> {
+    const s = await this.getNodeData(key)
+    return s.split(' ')
+  }
+
+  public async _addAddr(account: string, key: string, value: string): Promise<void> {
+    const s = await this.getNodeData(key)
+    if (s.includes(value)) {
+      throw new Error(`${value} already exists in ${key}`)
+    }
+    await this.setNodeData(account, key, `${s} ${value}`)
+  }
+
+  public async setNodeData(account: string, key: string, value: string): Promise<void> {
+    await this.setData(this.nftAddress, account, key, value)
+  }
+
+  public async getNodeData(key: string): Promise<string> {
+    return await this.getData(this.nftAddress, key)
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './themap'
 export * from './web3'
 export * from './utils'
+export * from './Node'
