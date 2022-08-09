87429fda81b2a704e7b76aec72e2dea76ce0228d
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-08-09 19:24:20 +0200

    remove querySearch() function

--- src/NodeSearch.ts
+++ src/NodeSearch.ts
@@ -2,31 +2,6 @@ import { Aquarius, Config, ConfigHelper } from '@oceanprotocol/lib'
 import { web3 } from './web3'
 
 export class NodeSearch {
-  public async querySearch(config: Config, query: any, signal?: AbortSignal) {
-    const aquarius = new Aquarius(config.metadataCacheUri)
-
-    const path = aquarius.aquariusURL + '/api/aquarius/assets/query'
-
-    try {
-      const response = await fetch(path, {
-        method: 'POST',
-        body: JSON.stringify(query),
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        signal
-      })
-
-      if (response.ok) {
-        return response.json()
-      } else {
-        throw new Error('querySearch failed: ' + response.status + response.statusText)
-      }
-    } catch (error) {
-      throw new Error('Error querying metadata: ' + error)
-    }
-  }
-
   public async search(searchQuery: any) {
     const chainId: number = await web3.eth.getChainId()
     const config: Config = new ConfigHelper().getConfig(chainId)
