4c6c8f57a92ac2becdf430d27bd6921da731454c
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-09-09 16:27:03 +0200

    export default in index.js

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-export * from './web3'
-export * from './utils'
-export * from './Node'
-export * from './NodeFactory'
-export * from './NodeSearch'
+import { web3 } from './web3'
+import { getCurrentAccount } from './utils'
+import { INBOUND_KEY, OUTBOUND_KEY, Node } from './Node'
+import { NodeFactory } from './NodeFactory'
+import { NodeSearch } from './NodeSearch'
+
+export default {
+  web3,
+  getCurrentAccount,
+  INBOUND_KEY,
+  OUTBOUND_KEY,
+  Node,
+  NodeFactory,
+  NodeSearch
+}
